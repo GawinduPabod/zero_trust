ff0055c086ceedc0bcef05c6bd6f41064e449cfa
GawinduPabod <pabothavindu@gmail.com>
2026-06-01 13:06:08 -0700

    Fixed internal server error and renamed template

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def get_db_connection():
 
 @app.route('/')
 def home():
-    return render_template('login.html')
+    return render_template('index.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
